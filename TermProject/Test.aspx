7531188a10877fe84944ad111db5e5ceb40efcf6
Du Ha <tug52950@temple.edu>
2019-04-04 20:50:30 -0700

    commit

--- TermProject/Test.aspx
+++ TermProject/Test.aspx
@@ -9,6 +9,58 @@
 <body>
     <form id="form1" runat="server">
         <div>
+            <h1>GET DEPARTMENT API</h1>
+            <br />
+            <asp:GridView ID="gvDepartment" runat="server">
+            </asp:GridView>
+            <br />
+            <asp:Button ID="btnGetDepartment" runat="server" OnClick="btnGetDepartment_Click" Text="Get Department" />
+            <br />
+
+            <h1>GET PRODUCTS BY DEPARTMENT NUMBER</h1>
+            <br />
+            <asp:DropDownList ID="ddlDepartment" runat="server">
+                <asp:ListItem Value="1">Amazon Devices</asp:ListItem>
+                <asp:ListItem Value="2">Books</asp:ListItem>
+                <asp:ListItem Value="3">Computer</asp:ListItem>
+                <asp:ListItem Value="4">Electronic</asp:ListItem>
+                <asp:ListItem Value="5">Home and Kitchen</asp:ListItem>
+            </asp:DropDownList>
+            <br />
+            <asp:GridView ID="gvProducts" runat="server">
+            </asp:GridView>
+            <br />
+            <asp:Button ID="btnGetProduct" runat="server" OnClick="btnGetProduct_Click" Text="Get Product by Department Number" />
+            <br />
+            <h1>REGISTER MERCHANT API</h1>
+            <p>
+                <asp:Label ID="Label1" runat="server" Text="Username"></asp:Label>
+&nbsp;&nbsp;&nbsp;
+                <asp:TextBox ID="txtUsername" runat="server"></asp:TextBox>
+            </p>
+            <p>
+                <asp:Label ID="Label2" runat="server" Text="Description"></asp:Label>
+&nbsp;&nbsp;&nbsp;
+                <asp:TextBox ID="txtDesc" runat="server"></asp:TextBox>
+            </p>
+            <p>
+                <asp:Label ID="Label3" runat="server" Text="Email"></asp:Label>
+&nbsp;&nbsp;&nbsp;
+                <asp:TextBox ID="txtEmail" runat="server"></asp:TextBox>
+            </p>
+            <p>
+                <asp:Label ID="Label4" runat="server" Text="Phone"></asp:Label>
+&nbsp;&nbsp;&nbsp;
+                <asp:TextBox ID="txtPhone" runat="server"></asp:TextBox>
+            </p>
+            <p>
+                <asp:Label ID="lblNotify" runat="server" ForeColor="Red" Text="Label" Visible="False"></asp:Label>
+            </p>
+            <p>
+                <asp:Button ID="btnAddMerchant" runat="server" OnClick="btnAddMerchant_Click" Text="Submit" />
+            </p>
+            <p>&nbsp;</p>
+            <p>&nbsp;</p>
         </div>
     </form>
 </body>
--- TermProject/Test.aspx.cs
+++ TermProject/Test.aspx.cs
@@ -5,13 +5,100 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+
+using System.Web.Script.Serialization;
+using System.IO;
+using System.Net;
+using Utilities;
+using ClassLibrary;
+
 namespace TermProject
 {
     public partial class Test : System.Web.UI.Page
     {
+        string url = "";
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void btnGetDepartment_Click(object sender, EventArgs e)
+        {
+            WebRequest request = WebRequest.Create(url);
+            WebResponse response = request.GetResponse();
+
+            Stream theDataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(theDataStream);
+            String data = reader.ReadToEnd();
+            reader.Close();
+            response.Close();
+
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            Department[] departments = js.Deserialize<Department[]>(data);
+
+            gvDepartment.DataSource = departments;
+            gvDepartment.DataBind();
+
+        }
+
+        protected void btnGetProduct_Click(object sender, EventArgs e)
+        {
+            int departmentnumber = Int32.Parse(ddlDepartment.SelectedValue);
+            url = url + "GetProductCatalog/" + departmentnumber.ToString();
+
+            WebRequest request = WebRequest.Create(url);
+            WebResponse response = request.GetResponse();
+
+            Stream theDataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(theDataStream);
+            String data = reader.ReadToEnd();
+            reader.Close();
+            response.Close();
+
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            Product[] products = js.Deserialize<Product[]>(data);
+
+            gvProducts.DataSource = products;
+            gvProducts.DataBind();
+        }
+
+        protected void btnAddMerchant_Click(object sender, EventArgs e)
+        {
+            Merchant merchant = new Merchant();
+
+            merchant.Seller_site = txtUsername.Text;
+            merchant.Desc = txtDesc.Text;
+            merchant.Email = txtEmail.Text;
+            merchant.Phone = txtPhone.Text;
+
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            string jsonMerchant = js.Serialize(merchant);
+
+            WebRequest request = WebRequest.Create(url);
+            request.Method = "POST";
+            request.ContentLength = jsonMerchant.Length;
+            request.ContentType = "application/json";
+
+            StreamWriter writer = new StreamWriter(request.GetRequestStream());
+            writer.Write(jsonMerchant);
+            writer.Flush();
+            writer.Close();
+
+            WebResponse response = request.GetResponse();
+            Stream theDataStream = response.GetResponseStream();
+            StreamReader streamReader = new StreamReader(theDataStream);
+            String data = streamReader.ReadToEnd();
+            streamReader.Close();
+            response.Close();
+
+            if (data == "true")
+            {
+                lblNotify.Text = "Successfully added a merchant!";
+            }
+            else
+            {
+                lblNotify.Text = "An error occurred! Please try again";
+            }
         }
     }
 }
