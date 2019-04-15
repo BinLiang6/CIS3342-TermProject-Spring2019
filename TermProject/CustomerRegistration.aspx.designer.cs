c7921ca1dcbbb003db761bc5d9e8e2e2ebee25e4
Du Ha <tug52950@temple.edu>
2019-04-15 11:49:02 -0700

    commit

--- TermProject/CustomerRegistration.aspx.designer.cs
+++ TermProject/CustomerRegistration.aspx.designer.cs
@@ -103,6 +103,15 @@ namespace TermProject {
         protected global::System.Web.UI.WebControls.TextBox txtSq3;
         
         /// <summary>
+        /// lblSuccess control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblSuccess;
+        
+        /// <summary>
         /// lblNotif control.
         /// </summary>
         /// <remarks>
@@ -119,5 +128,14 @@ namespace TermProject {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button btnSubmit;
+        
+        /// <summary>
+        /// btnReturnLogin control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnReturnLogin;
     }
 }
