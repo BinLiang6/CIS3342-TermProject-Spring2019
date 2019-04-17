48c2c9a36e767c9f4a7873b4bea9788947d143df
Du Ha <tug52950@temple.edu>
2019-04-16 17:16:30 -0700

    commit

--- TermProject/ShoppingSite.aspx
+++ TermProject/ShoppingSite.aspx
@@ -9,6 +9,13 @@
 <body>
     <form id="form1" runat="server">
         <div>
+
+            <asp:DropDownList ID="ddlDepartment" runat="server">
+            </asp:DropDownList>
+            <br />
+            <asp:GridView ID="GridView1" runat="server">
+            </asp:GridView>
+
         </div>
     </form>
 </body>
--- TermProject/ShoppingSite.aspx.cs
+++ TermProject/ShoppingSite.aspx.cs
@@ -5,12 +5,28 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+using Utilities;
+using System.Data;
+using System.Data.SqlClient;
+using ClassLibrary;
+
 namespace TermProject
 {
     public partial class ShoppingSite : System.Web.UI.Page
     {
+        DBConnect objdb = new DBConnect();
+        SqlCommand objcomm = new SqlCommand();
         protected void Page_Load(object sender, EventArgs e)
         {
+            objcomm.CommandType = CommandType.StoredProcedure;
+            objcomm.CommandText = "TP_GetDepartments";
+
+            ddlDepartment.DataSource = objdb.GetDataSetUsingCmdObj(objcomm);
+            ddlDepartment.DataTextField = "name";
+            ddlDepartment.DataValueField = "department_id";
+
+            ddlDepartment.DataBind();
+
 
         }
     }
